e5d2381e394a9296819ef178a92bf9ac171d3ba0
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-04-11 15:48:25 +0700

    Add code on index.js to try calculation

---
+++ index.js
@@ -0,0 +1,22 @@
+const numA = 50;
+const numB = 100;
+const numC = 70;
+const numD = 5;
+const numE = 10;
+const numF = 2;
+const numG = 2.5;
+const numH = 0.5;
+
+const addition = numA + numB;
+const substraction = numC - numD;
+const multiplication = numE * numF;
+const division = numG / numH;
+const mixResults1 = addition - substraction;
+const mixResults2 = multiplication * division;
+
+console.log(addition);
+console.log(substraction);
+console.log(multiplication);
+console.log(division);
+console.log(mixResults1);
+console.log(mixResults2);
